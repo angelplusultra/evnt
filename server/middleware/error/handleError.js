43dc986b4fdef701b20e5dffc9c7d059a43e6abc
Mac Fitton <marcellofitton@gmail.com>
2023-01-09 17:29:16 -0800

    update

--- server/middleware/error/handleError.js
+++ server/middleware/error/handleError.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 function handleError(err, req, res, next) {
+  const { NODE_ENV } = process.env;
   const code = res.statusCode ? res.statusCode : 500;
-  res.status(code).json({ message: err.message, stack: err.stack });
+  res.status(code).json({ message: err.message, stack: NODE_ENV === 'prod' ? undefined : err.stack });
 }
 
 export default handleError;
--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,12 @@ const router = Router();
 router.use('/api', apiRoutes);
 router.use('/auth', authRoutes);
 
+if (process.env.NODE_ENV === 'dev') {
+  router.get('/error', (req, res, next) => {
+    next(new Error('This is a test error'));
+  });
+}
+
 router.all('*', (req, res) => {
   res.status(404);
   throw new Error('404');
